73edeb0f8f31d3a65575978f07db2af80a845a1a
Valentina <105123172+m0nes1@users.noreply.github.com>
2022-11-23 18:05:09 +1300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+let arr = ['shaxzod', 'valentina', 'aziz', 'odilbek',  'emil', 'odilbek']
+let i = 0
+let name = ''
+
+while(name  !== 'abbos') {
+  if(arr[i] === 'abbos') {
+  name = arr[i]
+  console.log('Good ' + name);
+  i++
+}
+
+
+
+else if  ('abbos' !== arr[arr] || 'abbos' !== arr[i] ) 
+  
+    console.log('Sorry, abbos not found');
+
+
+
+
+
+    break
+    
+} 
+
+  
+
+
+//   console.log('Sorry, abbos not found');
+//   setTimeout(() => {
+    
+//   }, 2000);
+//   break
+// }
+
+
+
+
+// if (arr[arr.includes('abbos')] ) {
+//   console.log('good'); 
+// } else {
+ 
+//     console.log('Sorry we didnt find abbos');
+//   }
+
+
+// }
+
+  
+
+
+
+
+   
+   
+     
+
+        
+
+    
+
+
+
